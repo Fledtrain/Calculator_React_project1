615a89eb22d1ddc3d98af766269f7d2e0ab77c7a
Joey <Joeyew2014@gmail.com>
2022-09-24 23:06:45 -0400

    Started Creating calculator Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      {/* JSX doesnt use class, it uses className */}
-      <header className="App-header">
-        <p>
-          This is React
-        </p>
-        <a>
-          Its just like using regular HTML but easier 
-        </a>
-      </header>
+    <div className="calculator-grid">
+      {/* ^^ This is for the grid of the calculator ^^ */}
+      <div className="output">
+        {/* ^^ This is for the output of the calculator ^^*/}
+        <div className="previous-operand"></div>
+        <div className="current-operand"></div>
+      </div>
+      {/* Row 1 of calculator  */}
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      {/* Row 2 of calculator  */}
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      {/* Row 3 of calculator  */}
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      {/* Row 4 of calculator  */}
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      {/* Row 5 of calculator  */}
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
     </div>
   );
 }
-function test(){
-
-}
-
 
 export default App;
